e4f19bdf26f238cbd8be58b30295b54859c759a7
Joo <rudwn3166@naver.com>
2021-10-11 21:26:55 +0900

    room

---
+++ app/src/main/java/com/example/roomtour/datas/RoomClass.kt
@@ -0,0 +1,7 @@
+package com.example.roomtour.datas
+
+class RoomClass(val price : Int,
+                val address:String,
+                val floor:Int,
+                val description:String) {
+}
