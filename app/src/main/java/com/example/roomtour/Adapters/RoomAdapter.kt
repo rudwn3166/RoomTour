8d7f55642a0aa5180a9d5fce603b678a756ec4c0
Joo <rudwn3166@naver.com>
2021-10-12 20:03:40 +0900

    메인 어댑터 연결 및 데이터 작업

--- app/src/main/java/com/example/roomtour/Adapters/RoomAdapter.kt
+++ app/src/main/java/com/example/roomtour/Adapters/RoomAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.roomtour.Adapters
 
-class RoomAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.roomtour.R
+import com.example.roomtour.datas.RoomClass
+
+class RoomAdapter(val mContext: Context,
+                  val resId:Int,
+                  val mList:ArrayList<RoomClass>) : ArrayAdapter<RoomClass>(mContext, resId,mList) {
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var temprow = convertView
+
+        if(temprow == null){
+
+            temprow=inflater.inflate(R.layout.room_list_item,null)
+
+
+        }
+
+        var row = null!!
+
+        return row
+
+    }
 }
--- app/src/main/java/com/example/roomtour/MainActivity.kt
+++ app/src/main/java/com/example/roomtour/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.roomtour
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.roomtour.Adapters.RoomAdapter
+import com.example.roomtour.datas.RoomClass
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomClass>()
+
+    lateinit var mAdapter : RoomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add( RoomClass( 7500, "서울시 동대문구", 8, "동대문구의 8층 7500만원 방 입니다." ) )
+        mRoomList.add( RoomClass( 24500, "서울시 서대문구", 0, "서대문구의 반지하 2억 4500만원 방 입니다." ) )
+        mRoomList.add( RoomClass( 8500, "서울시 동작구", 0, " 동작구의 반지하 8500만원 방 입니다." ) )
+        mRoomList.add( RoomClass( 4500, "서울시 은평구", -2, " 은평구의 지하 2층 4500만원 방 입니다." ) )
+        mRoomList.add( RoomClass( 182500, "서울시 중랑구", 5, "중랑구의 5층 1억 18억 2500만원 방 입니다." ) )
+        mRoomList.add( RoomClass( 235000, "서울시 도봉구", 7, "도봉구의 7층 1억 23억 5000만원 방 입니다." ) )
+        mRoomList.add( RoomClass( 24000, "서울시 강남구", 19, "강남구의 19층 2억 4000만원 방 입니다." ) )
+        mRoomList.add( RoomClass( 3700, "서울시 서초구", -1, "서초구의 지하 1층 3700만원 방 입니다." ) )
+
+        mAdapter =RoomAdapter(this,R.layout.room_list_item,mRoomList)
+        main_list.adapter =mAdapter
+
     }
 }
