6a0b5f0d5936960de92b99881619ded4e553d111
Joo <rudwn3166@naver.com>
2021-10-13 19:36:10 +0900

    디테일 방 보여주는 화면 작성

---
+++ app/src/main/java/com/example/roomtour/RoomShowDetail_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.roomtour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomShowDetail_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_show_detail)
+    }
+}
